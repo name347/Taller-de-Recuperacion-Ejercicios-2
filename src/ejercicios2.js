b9f7d26d6d2993761703d8348f72f5a280f01474
Santiago Bejarano <bejaranosantiago77@gmail.com>
2026-03-11 14:48:07 -0500

    Cargar ejercicio 2

--- src/ejercicios2.js
+++ src/ejercicios2.js
@@ -27,6 +27,19 @@ function clasificarTriangulo(lado1, lado2, lado3) {
 	// - "Escaleno" si todos los lados son diferentes
 	// - "No es triángulo" si no cumple la propiedad triangular
 	// Tu código:
+		if (lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1) {
+		return "No es triángulo";
+	}
+
+	if (lado1 === lado2 && lado2 === lado3) {
+		return "Equilátero";
+	}
+
+	if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+		return "Isósceles";
+	}
+
+	return "Escaleno";
 }
 
 // Test 3:
