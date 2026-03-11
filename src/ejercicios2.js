412719ced10bef9d7780dbe06b2f0af925c6f2a3
Santiago Bejarano <bejaranosantiago77@gmail.com>
2026-03-11 09:36:26 -0500

    nombre del ejercicio resuleto

--- src/ejercicios2.js
+++ src/ejercicios2.js
@@ -15,6 +15,7 @@ function esMayorDeEdad(edad) {
 	// Recibe un número representando la edad de una persona.
 	// Devuelve true si la edad es mayor o igual a 18, false en caso contrario.
 	// Tu código:
+	return edad >= 18;
 }
 
 // Test 2:
