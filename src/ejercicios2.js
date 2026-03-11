309e81b6ebc2b82697c906337a54afb90fcc3f71
Santiago Bejarano <bejaranosantiago77@gmail.com>
2026-03-11 16:29:15 -0500

    Cargar ejercicio 4

--- src/ejercicios2.js
+++ src/ejercicios2.js
@@ -72,6 +72,33 @@ function obtenerDiaSemana(numero) {
 	// 1 = "Lunes", 2 = "Martes", ..., 7 = "Domingo"
 	// Si el número no está en el rango, devuelve "Día inválido"
 	// Tu código:
+			switch (numero) {
+
+		case 1:
+			return "Lunes";
+
+		case 2:
+			return "Martes";
+
+		case 3:
+			return "Miércoles";
+
+		case 4:
+			return "Jueves";
+
+		case 5:
+			return "Viernes";
+
+		case 6:
+			return "Sábado";
+
+		case 7:
+			return "Domingo";
+
+		default:
+			return "Día inválido";
+	}
+	
 }
 
 // Test 5:
